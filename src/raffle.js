f7f253a8782af52fddcb85d7a1d90d9cc648f093
Pratush Bhandari <pratush.bh@gmail.com>
2021-10-26 19:49:34 +0530

    Create local contract instance

---
+++ src/raffle.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+
+const address = '0x2596094CECf592Eb0024226e33aAC2E7241f11E3'
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi, address);
