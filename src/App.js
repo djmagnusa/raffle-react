4db76ea6cc169bba4756077bfe4fe03a9dfe9343
Pratush Bhandari <pratush.bh@gmail.com>
2021-10-27 00:39:41 +0530

    Add pickWinner functionality

--- src/App.js
+++ src/App.js
@@ -33,10 +33,21 @@ class App extends React.Component {
     });
 
     this.setState({message: 'You have been entered! '});
+  };
+
+  onClick = async () => {
+     const accounts = await web3.eth.getAccounts();
+
+     this.setState({ message: 'Waiting on transaction success...'});
 
 
 
-  }
+     await raffle.methods.pickWinner().send({
+        from: accounts[0]
+     })
+
+     this.setState({ message: 'A winner has been picked!'});
+  };
 
   
   render() {
@@ -64,6 +75,11 @@ class App extends React.Component {
           </div>
           <button>Enter</button>
         </form>
+
+        <hr />
+
+        <h4>Ready to pick a winner!</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
 
         <hr />
 
